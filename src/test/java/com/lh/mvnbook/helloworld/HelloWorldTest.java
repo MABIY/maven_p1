a340cc4a2dafeb1021bdb0c48b48ef5dd5a6501e
skylark <jyfc7879@gmail.com>
2018-03-06 11:54:16 +0800

    first commit

---
+++ src/test/java/com/lh/mvnbook/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.lh.mvnbook.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHello();
+        assertEquals("Hello Maven", result);
+    }
+}
